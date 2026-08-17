0af361282bff308381c7611fb249de81cd12316f
Anonymous 🎭 <157519745+anonymous-beta@users.noreply.github.com>
2026-08-17 21:40:44 +0100

    Delete core directory

--- core/formatter.py
+++
@@ -1,4 +0,0 @@
-import json
-
-def prettify(title, data):
-    return f"\n{'='*10} {title} {'='*10}\n{json.dumps(data, indent=2)}\n"
--- core/meta_extract.py
+++
@@ -1,6 +0,0 @@
-def extract(file_path: str) -> dict:
-    """Mock metadata extraction"""
-    return {
-        "filename": file_path.split("/")[-1],
-        "exif": None
-    }
